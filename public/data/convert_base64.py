6275a5c00e16da48960b23a0309871bdca4118ba
miuraK <kanae9@icloud.com>
2021-05-17 18:18:03 +0900

    ファイル削除

--- public/data/convert_base64.py
+++
@@ -1,5 +0,0 @@
-import base64
-
-client_id = "b094df9818f245aba74ada3925a663ed"
-client_secret = "fd2a2adedce44539868b565adb79a9f8"
-print(base64.b64encode((client_id+":"+client_secret).encode("utf-8")))
