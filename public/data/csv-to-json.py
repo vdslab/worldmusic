2731e7a15e3b167dbd4e161237e551bbd5fd9475
miuraK <kanae9@icloud.com>
2021-05-17 22:15:07 +0900

    アメリカランキングデータ追加(20180112まで)

--- public/data/csv-to-json.py
+++ public/data/csv-to-json.py
@@ -3,12 +3,12 @@ import csv
 
 json_list = []
 
-with open('aa9.csv', 'r') as f: 
+with open('aa8.csv', 'r') as f: 
     csv_reader = csv .DictReader(f)
     for row in csv_reader:
         json_list.append(row)
 
 
-with open('aaa9.json', 'w') as f: 
+with open('aaa8.json', 'w') as f: 
     json.dump(json_list, f, ensure_ascii=False,
               indent=4, separators=(',', ': '))
