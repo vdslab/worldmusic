6cbdc96a668a9f6293dca1a11606ee63f10ccbe3
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-06-06 00:14:18 +0900

    update

--- public/get_rankingtop.py
+++ public/get_rankingtop.py
@@ -1,30 +1,31 @@
 import codecs
 import csv
 import pandas as pd
+import os
 
-folder = './data/data_by_country/America/'
+folder = './data/data_by_country/germany/'
 file = 'America-2016-12-23--2016-12-30-details.csv'
 
-s = 0
-f = 15
+filelist =os.listdir('data/data_by_country/germany')
 
 csv_file = []
 datas = []
 
-with codecs.open(folder+file, "r", encoding='utf-8',errors='ignore') as f:
+for file in filelist:
+    print(file)
+    with codecs.open(folder+file, "r", encoding='utf-8',errors='ignore') as f:
         reader = csv . reader(f)
         for line in reader:
             line.pop(0)
             csv_file.append(line)
-
-del csv_file[0]
-for i in range(0,15):
-    datas.append(csv_file[i])
+        del csv_file[0]
+        for i in range(0,10):
+            datas.append(csv_file[i])
 
 df = pd.DataFrame(datas,columns=['name', 'acousticness', 'danceability', 'energy', 'instrumentalness',
                                       'liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'time_signature', 'valence', 'streams'])
     
 
-filename = 'top'+'-'+file
+filename = 'top10'+'-'+'Germany.csv'
 
 df.to_csv(filename)
