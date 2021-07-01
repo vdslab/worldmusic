ac4141e9052cba3d9b2a61c33b33502b0ca9ee7e
miuraK <kanae9@icloud.com>
2021-07-01 19:23:02 +0900

    世界地図の表示

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { DrowWorldMap } from "./drow_worldmap";
 
 const WorldMap = () => {
   return (
     <div className="my-section">
       <div className="card" style={{ height: "40vh" }}>
         <div className="card-content">
-          <div className="content">世界地図</div>
+          <div className="content">
+            <div className="select is-small">
+              <select>
+                <option>acousticness</option>
+                <option>danceability</option>
+                <option>energy</option>
+                <option>instrumentalness</option>
+                <option>liveness</option>
+                <option>loudness</option>
+                <option>mode</option>
+                <option>speechiness</option>
+                <option>tempo</option>
+                <option>time_signature</option>
+                <option>valence</option>
+              </select>
+            </div>
+            <DrowWorldMap />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/drow_worldmap.js
@@ -0,0 +1,71 @@
+import {useEffect, useState} from "react";
+import  * as d3 from "d3";
+import { select } from 'd3-selection';
+import * as topojson from "topojson";
+
+const WorldMap = ({ features }) => {
+    const margin ={
+        top:20,
+        bottom: 75,
+        left :100,
+        right:150
+    };
+    const width = 960;
+    const height = 500;
+    const centerPos = [0,0];
+    const scale = 100;
+  
+    const projection = d3.geoMercator()
+                         .center(centerPos)
+                         .translate([width/2,height/2])
+                         .scale(scale);
+    
+    const path = d3.geoPath().projection(projection);
+
+    const svgWidth = margin.left+margin.right+width;
+    const svgHeight = margin.bottom+margin.top+height;
+    const [color,setColor] = useState("white");
+          
+    return (
+        <div select="#map-container">
+            <svg  viewBox={`${-margin.left} ${-margin.top} ${svgWidth} ${svgHeight}`}>
+            <g>
+          {features.map(item => (
+              <path
+                d={path(item)}
+                fill="white"  //{Color(item)}
+                stroke="black"
+                strokeWidth="1"
+                strokeOpacity="0.5"
+                onMouseOver={(e) => {
+                  select(e.target)
+                    .attr('stroke', 'red')
+                }}
+                onMouseOut={(e) => {
+                  select(e.target)
+                    .attr('stroke', 'black')
+                }}
+              />
+            ))
+          }
+        </g>
+            </svg>
+        </div>
+    );
+}
+
+export const DrowWorldMap = () => {
+    const [features, setFeatures] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`${process.env.PUBLIC_URL}/data/worldmap.json`);
+            const data = await res.json()
+            const { features } = topojson.feature(data, data.objects.worldmap);
+            setFeatures(features);
+        })();
+    }, []);
+    if (features == null) {
+        return <p>loading</p>;
+    }
+    return <WorldMap features={features}/>;
+}
