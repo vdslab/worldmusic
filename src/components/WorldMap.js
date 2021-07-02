9113a810f4b683887de6c0709aea75714eef5b49
miuraK <kanae9@icloud.com>
2021-07-03 01:33:10 +0900

    スライダー・プルダウンの追加＋値取得

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,36 +1,47 @@
 import React from "react";
 import { DrowWorldMap } from "./drow_worldmap";
+import { useState } from "react";
 
 const WorldMap = () => {
+  const [year, setValue] = useState(0);
+  const [start,setStart] = useState("2017-01");
+  const [end,setEnd] = useState("2017-06");
+  const elements = ["acousticness","danceability","energy","instrumentalness","liveness","loudness","mode","speechiness","tempo","time_signature","valence"];
+  const textYear = [["2017-01","2017-06"],["2017-07","2017-12"],["2018-01","2018-06"],["2018-07","2018-12"],["2019-01","2019-06"],["2019-07","2019-12"],["2020-01","2020-06"],["2020-07","2020-12"]];
+  //let start = textYear[0][0];
+  //let end = textYear[0][1];
+
   return (
     <div className="my-section">
       <div className="card" style={{ height: "40vh" }}>
         <div className="card-content">
           <div className="content">
             <div className="select is-small">
-              <select>
-                <option>acousticness</option>
-                <option>danceability</option>
-                <option>energy</option>
-                <option>instrumentalness</option>
-                <option>liveness</option>
-                <option>loudness</option>
-                <option>mode</option>
-                <option>speechiness</option>
-                <option>tempo</option>
-                <option>time_signature</option>
-                <option>valence</option>
+              <select onChange={(event) => {console.log(event.target.value);}}>
+                {elements.map((element,i) => {
+                  return(
+                    <option>{element}</option>
+                  );
+                })}
               </select>
             </div>
-            <DrowWorldMap />
-            <input
-              class="slider is-fullwidth"
-              step="1"
-              min="0"
-              max="100"
-              value="50"
-              type="range"
+            <input 
+              className="slider is-fullwidth" type="range" id="getSliderValue"
+              min="0" max="7" step="1"
+              value={year}
+              onChange={(event) => {
+                //console.log(event.target.value);
+                setValue(event.target.value);
+                setStart(textYear[event.target.value][0]);
+                setEnd(textYear[event.target.value][1]);
+                //start = textYear[event.target.value][0];
+                //end = textYear[event.target.value][1];
+                //console.log(start);
+                //console.log(end);
+              }}
             ></input>
+            <output for="sliderWithValue">{start}〜{end}</output>
+            <DrowWorldMap />
           </div>
         </div>
       </div>
