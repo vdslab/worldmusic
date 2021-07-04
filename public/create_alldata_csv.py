e8b4baa1f3e79d38b22c68ee9412ee332a2b38ae
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-07-05 02:35:08 +0900

    delete file

--- public/create_alldata_csv.py
+++
@@ -1,9 +0,0 @@
-from get_alldata import get_json
-import os
-
-folders = os.listdir("./data/ranking/AU")
-# for file in folders:
-#     csv_path = "./data/ranking/AU/"+file
-#     print(csv_path)
-get_json("./data/ranking/GL/regional-global-weekly-2016-12-23--2016-12-30.csv")
-
