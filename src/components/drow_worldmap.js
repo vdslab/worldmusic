906966e141ffdc532ffee8ef8cee34ac2fa2d526
miuraK <kanae9@icloud.com>
2021-07-07 01:30:10 +0900

    カラーバー追加

--- src/components/drow_worldmap.js
+++ src/components/drow_worldmap.js
@@ -121,6 +121,56 @@ const WorldMap = ({ features }) => {
             />
           ))}
           </g>
+          <g transform="translate(650,260) rotate(-90)">
+            <defs>
+              <linearGradient id="gradient">
+                <stop offset="0%" stop-color={d3.interpolateTurbo(0)} />
+                <stop offset="5%" stop-color={d3.interpolateTurbo(0.05)} />
+                <stop offset="10%" stop-color={d3.interpolateTurbo(0.1)} />
+                <stop offset="15%" stop-color={d3.interpolateTurbo(0.15)} />
+                <stop offset="20%" stop-color={d3.interpolateTurbo(0.2)} />
+                <stop offset="25%" stop-color={d3.interpolateTurbo(0.25)} />
+                <stop offset="30%" stop-color={d3.interpolateTurbo(0.3)} />
+                <stop offset="35%" stop-color={d3.interpolateTurbo(0.35)} />
+                <stop offset="40%" stop-color={d3.interpolateTurbo(0.4)} />
+                <stop offset="45%" stop-color={d3.interpolateTurbo(0.45)} />
+                <stop offset="50%" stop-color={d3.interpolateTurbo(0.5)} />
+                <stop offset="55%" stop-color={d3.interpolateTurbo(0.55)} />
+                <stop offset="60%" stop-color={d3.interpolateTurbo(0.6)} />
+                <stop offset="65%" stop-color={d3.interpolateTurbo(0.65)} />
+                <stop offset="70%" stop-color={d3.interpolateTurbo(0.7)} />
+                <stop offset="75%" stop-color={d3.interpolateTurbo(0.75)} />
+                <stop offset="80%" stop-color={d3.interpolateTurbo(0.8)} />
+                <stop offset="85%" stop-color={d3.interpolateTurbo(0.85)} />
+                <stop offset="90%" stop-color={d3.interpolateTurbo(0.9)} />
+                <stop offset="95%" stop-color={d3.interpolateTurbo(0.95)} />
+                <stop offset="100%" stop-color={d3.interpolateTurbo(1)} />
+              </linearGradient>
+              </defs>
+              <rect x="0" y="10" width="300" height="20" fill="url('#gradient')" />
+              <line x1="0" y1="30" x2="0" y2="40" stroke="black" />
+              <text x="0" y="50" font-size="10" text-anchor="start">0</text>
+              <line x1="30" y1="30" x2="30" y2="40" stroke="black" />
+              <text x="30" y="50" font-size="10" text-anchor="start">0.1</text>
+              <line x1="60" y1="30" x2="60" y2="40" stroke="black" />
+              <text x="60" y="50" font-size="10" text-anchor="start">0.2</text>
+              <line x1="90" y1="30" x2="90" y2="40" stroke="black" />
+              <text x="90" y="50" font-size="10" text-anchor="start">0.3</text>
+              <line x1="120" y1="30" x2="120" y2="40" stroke="black" />
+              <text x="120" y="50" font-size="10" text-anchor="start">0.4</text>
+              <line x1="150" y1="30" x2="150" y2="40" stroke="black" />
+              <text x="150" y="50" font-size="10" text-anchor="start">0.5</text>
+              <line x1="180" y1="30" x2="180" y2="40" stroke="black" />
+              <text x="180" y="50" font-size="10" text-anchor="start">0.6</text>
+              <line x1="210" y1="30" x2="210" y2="40" stroke="black" />
+              <text x="210" y="50" font-size="10" text-anchor="start">0.7</text>
+              <line x1="240" y1="30" x2="240" y2="40" stroke="black" />
+              <text x="240" y="50" font-size="10" text-anchor="start">0.8</text>
+              <line x1="270" y1="30" x2="270" y2="40" stroke="black" />
+              <text x="270" y="50" font-size="10" text-anchor="start">0.9</text>
+              <line x1="300" y1="30" x2="300" y2="40" stroke="black" />
+              <text x="300" y="50" font-size="10" text-anchor="start">1</text>
+          </g>
         </svg>
       </div>
       <footer class="card-footer">
@@ -136,7 +186,7 @@ const WorldMap = ({ features }) => {
         </p>
         <p class="card-footer-item">
           <span>
-            色の詳細
+            色の詳細？
           </span>
         </p>
       </footer>
