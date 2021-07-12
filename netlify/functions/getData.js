0fdf978fc607ac1747d7f4291322130bada466ea
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-07-13 01:40:25 +0900

    get song data

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -18,8 +18,29 @@ exports.handler = async function (event) {
   const endMonth = event.queryStringParameters.endMonth || null;
   const feature = event.queryStringParameters.feature || null;
   const country = event.queryStringParameters.country || null;
+  const musicId = event.queryStringParameters.musicId || null;
 
   /**TODO:応急処置, 後でちゃんとした書き方先輩に聞く */
+  if (musicId !== null) {
+    const data = {
+      path: `SELECT Music.Musicid , Music.name , Music.acousticness, Music.danceability, Music.energy , Music.instrumentalness , Music.liveness, Music.loudness, Music.mode, Music.speechiness, Music.tempo, Music.time_signature, Music.valence FROM Music WHERE Music.musicid=${musicId}`,
+    };
+
+    console.log(data);
+
+    try {
+      const dbpath = "./netlify/functions/database.db";
+      const db = new sqlite3.Database(dbpath);
+
+      const result = await selectRows(
+        db,
+        `SELECT Music.Musicid , Music.name , Music.acousticness, Music.danceability, Music.energy , Music.instrumentalness, Music.liveness, Music.loudness,Music.mode, Music.speechiness, Music.tempo, Music.time_signature, Music.valence FROM Music WHERE Music.musicid='${musicId}'`
+      );
+      return { statusCode: 200, body: JSON.stringify(result) };
+    } catch (e) {
+      return { statusCode: 500, body: "error" };
+    }
+  }
 
   if (country !== "ALL") {
     const data = {
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { fetchData } from "../api";
 
 const Song = () => {
+  const musicId = "3eekarcy7kvN4yt5ZFzltW";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData("", "", "", "ALL", musicId);
+      setData(data);
+    })();
+  }, [musicId]);
+
+  console.log(data);
+
   return (
     <div className="my-section">
       <div className="card" style={{ height: "24.25vh" }}>
         <div className="card-content">
-          <div className="content">曲</div>
+          <div className="content">
+            <div>
+              <span>曲詳細 </span>
+              {data[0]?.name}
+            </div>
+            <div style={{ display: "flex" }}>
+              <div style={{ width: "40%" }}>
+                <ul>
+                  <li>テンポ：{data[0].tempo}</li>
+                  <li>拍子：{data[0].time_signature}</li>
+                  <li>調：{data[0].mode == 0 ? "短調" : "長調"}</li>
+                </ul>
+              </div>
+              <div style={{ width: "60%" }}>chart</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
