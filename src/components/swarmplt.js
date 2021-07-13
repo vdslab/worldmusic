a504e3489492782097d83ade47940b8ec61c990b
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-07-13 23:13:36 +0900

    update swarmplt

--- src/components/swarmplt.js
+++ src/components/swarmplt.js
@@ -12,19 +12,10 @@ import { extent } from "d3-array";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchData } from "../api";
 
-const sampleData = [
-  { x: 200, y: 150, r: 20, color: "red" },
-  { x: 200, y: 100, r: 20, color: "black" },
-  { x: 200, y: 50, r: 20, color: "green" },
-  { x: 290, y: 200, r: 15, color: "blue" },
-  { x: 270, y: 170, r: 10, color: "yellow" },
-];
-
 const Swarmplt = () => {
   const ref = useRef();
   const svg = select(ref.current);
 
-  const dispatch = useDispatch();
   const [dbData, setDbData] = useState([]);
 
   const startMonth = useSelector((state) => state.detail.startMonth);
@@ -39,30 +30,15 @@ const Swarmplt = () => {
       setDbData(data);
     })();
     console.log(dbData);
-    const jpData = dbData.filter((item) => {
-      return item.countryid == "JP";
-    });
-    const f = jpData.map((item) => {
-      return item.acousticness;
-    });
-    const stream = jpData.map((item) => {
-      return item.stream;
-    });
 
-    let fmax = Math.max(...f);
-    let fmin = Math.min(...f);
-    let streammax = Math.max(...stream);
-    let streammin = Math.min(...stream);
-    console.log(streammin, streammax);
-    // const yScale = scaleLinear().domain([fmin, fmax]).range([200, 10]);
-    let xScale = scaleLinear()
-      .domain(extent(jpData.map((d) => +d.acousticness)))
+    const xScale = scaleLinear()
+      .domain(extent(dbData.map((d) => +d.acousticness)))
       .range([750, 10]);
-    const rScale = scaleLinear().domain([streammin, streammax]).range([30, 5]);
-    let marketcapDomain = extent(jpData.map((d) => d.stream));
-    marketcapDomain = marketcapDomain.map((d) => Math.sqrt(d));
-    let size = scaleLinear().domain(marketcapDomain).range([1, 30]);
-    let simulation = forceSimulation(jpData)
+
+    let streamDomain = extent(dbData.map((d) => d.stream));
+    streamDomain = streamDomain.map((d) => Math.sqrt(d));
+    let size = scaleLinear().domain(streamDomain).range([1, 30]);
+    let simulation = forceSimulation(dbData)
       // .force(
       //   "x",
       //   forceX(() => {
@@ -88,7 +64,7 @@ const Swarmplt = () => {
       .on("tick", () =>
         svg
           .selectAll("circle")
-          .data(jpData)
+          .data(dbData)
           .join("circle")
           .style("fill", "red")
           .attr("stroke", "black")
@@ -101,14 +77,6 @@ const Swarmplt = () => {
 
   return (
     <div>
-      {/* <svg viewBox="0 0 1000 500">
-        <g>
-          {sampleData.map((d) => {
-            console.log(d.r);
-            return <circle cx="200" cy={d.y} r={d.r} fill={d.color}></circle>;
-          })}
-        </g>
-      </svg> */}
       <svg height="200" width="800" ref={ref} />
     </div>
   );
