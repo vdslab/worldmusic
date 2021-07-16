023b010706cf660d27ede6f2be8d159937b6d66a
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-07-16 16:00:08 +0900

    update

--- build/delete_firstRow.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-name = "Ranking"
-df = pd.read_csv("./data/"+name+".csv",dtype=object)
-print(df.columns)
-df = df.drop(columns='Unnamed: 0')
-#print(df)
-df.to_csv('./data/'+name+'2.csv',index=False,header=False)
