ee064f2b3787a10d231a6cf3dc672b3e8b5684d4
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-07-17 19:14:31 +0900

    test update

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -14,26 +14,33 @@ function selectRows(db, sql) {
 }
 
 exports.handler = async function (event) {
-  const startMonth = event.queryStringParameters.startMonth || null;
-  const endMonth = event.queryStringParameters.endMonth || null;
-  const feature = event.queryStringParameters.feature || null;
-  const country = event.queryStringParameters.country || null;
+  // const startMonth = event.queryStringParameters.startMonth;
+  // const endMonth = event.queryStringParameters.endMonth;
+  // const feature = event.queryStringParameters.feature;
+  // const country = event.queryStringParameters.country;
+  // console.log(startMonth, endMonth, feature, country);
+
+  const startMonth = event.queryStringParameters.startMonth;
+  const endMonth = event.queryStringParameters.endMonth;
+  const feature = "acousticness";
+  const country = "AU";
+  // console.log(startMonth, endMonth, feature, country);
 
   /**TODO:応急処置, 後でちゃんとした書き方先輩に聞く */
 
   try {
-    const dbpath = "./netlify/functions/example.db";
+    const dbpath = "./netlify/functions/database.db";
     const db = new sqlite3.Database(dbpath);
 
     const result = await selectRows(
       db,
-      // `SELECT Music.Musicid , Music.${feature} , Ranking.startday , Ranking.countryid , Ranking.stream FROM Music INNER JOIN Ranking ON Music.musicid=Ranking.musicid WHERE Ranking.countryid='${country}' AND Ranking.startday BETWEEN '${startMonth}-01' AND '${endMonth}-31'`
+      `SELECT Music.Musicid , Music.${feature} , Ranking.startday , Ranking.countryid , Ranking.stream FROM Music INNER JOIN Ranking ON Music.musicid=Ranking.musicid WHERE Ranking.countryid='${country}' AND Ranking.startday BETWEEN '${startMonth}-01' AND '${endMonth}-31'`
       // `SELECT Music.Musicid , Music.acousticness , Ranking.startday , Ranking.countryid , Ranking.stream FROM Music INNER JOIN Ranking ON Music.musicid=Ranking.musicid WHERE Ranking.countryid='AU' AND Ranking.startday BETWEEN '2017-01-01' AND '2017-03-31'`
-      "SELECT id, name FROM staffs"
+      // "SELECT id, name FROM staffs"
     );
     return { statusCode: 200, body: JSON.stringify(result) };
   } catch (e) {
-    return { statusCode: 500, body: "error" };
+    return { statusCode: 500, body: e.message };
   }
 };
 
