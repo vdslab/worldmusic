febed4149a37f9d01cf7b22a850fbb0e921bd2a7
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-07-21 04:05:51 +0900

    update

--- src/components/draw_swarmplt.js
+++ src/components/draw_swarmplt.js
@@ -37,28 +37,32 @@ const Swarmplt = ({ width, height }) => {
   useEffect(() => {
     let a = -Infinity;
     let b = Infinity;
-    (async () => {
-      const data = await fetchData(
-        startMonth,
-        endMonth,
-        feature,
-        country,
-        musicid
-      );
-      console.log(data);
-      data.map((item, i) => {
-        if (a < item[feature]) {
-          a = item[feature];
-        }
-        if (item[feature] < b) {
-          b = item[feature];
-        }
-        item[feature] = checkColor(item[feature]);
-      });
-      setDbData(data);
-      setMax(a);
-      setMin(b);
-    })();
+
+    [...Array(32)].map((_, i) => {
+      (async () => {
+        const data = await fetchData(
+          startMonth,
+          endMonth,
+          feature,
+          country,
+          musicid
+        );
+        console.log(data);
+      })();
+    });
+    // data.map((item, i) => {
+    //   if (a < item[feature]) {
+    //     a = item[feature];
+    //   }
+    //   if (item[feature] < b) {
+    //     b = item[feature];
+    //   }
+    //   item[feature] = checkColor(item[feature]);
+    // });
+    // setDbData(data);
+    // setMax(a);
+    // setMin(b);
+    // })();
     d3.select(ref.current)
       .attr("width", width)
       .attr("height", height)
