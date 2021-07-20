597472749dd795ae8bdcd5186ccdd0aea914abf9
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-07-20 13:22:11 +0900

    コンフリクト修正

--- src/components/draw_heatmap.js
+++ src/components/draw_heatmap.js
@@ -123,6 +123,8 @@ function HeatMapChart() {
   const [heatMapData, setHeatMapData] = useState([]);
   const [Max, setMax] = useState(-Infinity);
   const [Min, setMin] = useState(Infinity);
+  const [clicked, setClicked] = useState(-1);
+  const [pos, setPos] = useState(null);
 
   useEffect(() => {
     let Max = -Infinity;
@@ -241,25 +243,61 @@ function HeatMapChart() {
           />
 
           {/*<Legend h={contentWidth} w={contentWidth} />*/}
-
-          {heatMapData.map((country, i) => {
-            //console.log(country);
-            return country.timeData.map((item, j) => {
-              return (
-                <rect
-                  x={len * j}
-                  y={len * i}
-                  width={len}
-                  height={len}
-                  fill={colorjudge(item.value, item.start)}
-                  key={i * country.timeData.length + j}
-                  onClick={() => {
-                    changeInfo(item.start, item.end, country.countryName);
-                  }}
-                />
-              );
-            });
-          })}
+          <g
+            onMouseLeave={() => {
+              setPos(null);
+            }}
+          >
+            {heatMapData.map((country, i) => {
+              //console.log(country);
+              return country.timeData.map((item, j) => {
+                return (
+                  <g>
+                    <rect
+                      className="cell"
+                      x={len * j}
+                      y={len * i}
+                      width={len}
+                      height={len}
+                      fill={colorjudge(item.value, item.start)}
+                      onClick={() => {
+                        changeInfo(item.start, item.end, country.countryName);
+                        setClicked(i * country.timeData.length + j);
+                      }}
+                      onMouseEnter={() => {
+                        setPos({
+                          col: i,
+                          row: j,
+                          value: item.value?.toFixed(2) || " - ",
+                        });
+                        console.log(pos);
+                      }}
+                    />
+                    <rect
+                      x={len * j}
+                      y={len * i}
+                      width={len - 0.5}
+                      height={len - 0.5}
+                      fill="none"
+                      stroke="black"
+                      opacity={
+                        clicked === i * country.timeData.length + j ? 1 : 0
+                      }
+                    />
+                    {pos !== null ? (
+                      <g>
+                        <text x={len * pos.row} y={len * pos.col} fontSize={12}>
+                          {pos.value}
+                        </text>
+                      </g>
+                    ) : (
+                      []
+                    )}
+                  </g>
+                );
+              });
+            })}
+          </g>
         </svg>
       </div>
     </div>
