651c0ced0e334278230399f6e87aa9eee45c18c9
yk-hts <chyk18074@g.nihon-u.ac.jp>
2021-11-01 13:43:24 +0900

    update db

--- src/components/CountryHeatMap.js
+++ src/components/CountryHeatMap.js
@@ -1,12 +1,9 @@
 import React from "react";
 import HeatMapChart from "./draw_heatmap";
 import { useEffect, useState } from "react";
-import { fetchData, } from "../api";
+import { fetchData } from "../api";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  changeMax,
-  changeMin,
-} from "../stores/details";
+import { changeMax, changeMin } from "../stores/details";
 
 const CountryHeatMap = () => {
   const dispatch = useDispatch();
@@ -14,6 +11,7 @@ const CountryHeatMap = () => {
   const [heatMapData, setHeatMapData] = useState([]);
   const [Max, setMax] = useState(-Infinity);
   const [Min, setMin] = useState(Infinity);
+  const country = [];
   useEffect(() => {
     (async () => {
       const data = await fetchData(feature);
@@ -25,12 +23,18 @@ const CountryHeatMap = () => {
       //console.log(data);
     })();
   }, [feature]);
+
   return (
     <div className="card-content p-1">
       <div className="content">
-        チェックボックスの出現
-        （チェック押してからヒートマップの出現）
-        <HeatMapChart judgeNumber={2} data={heatMapData} max={Max} min={Min}/>
+        チェックボックスの出現 （チェック押してからヒートマップの出現）
+        <HeatMapChart
+          judgeNumber={2}
+          data={heatMapData}
+          max={Max}
+          min={Min}
+          y={country}
+        />
       </div>
     </div>
   );
