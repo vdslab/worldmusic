496985464d7db77af5d572dba53946893fe36c6f
miuraK <kanae9@icloud.com>
2021-12-01 17:05:14 +0900

    データベース関連

---
+++ public/insert_csv.py
@@ -0,0 +1,59 @@
+import sqlite3
+import csv
+import os
+
+dbname = './data/musicvisdatabase.db'
+target_table_name = 'Ranking'
+import_table_name = './data/'+target_table_name+'.csv'
+is_header_skip = False
+
+class ImportSQLite():
+    def __init__(self, dbname, target_table_name, import_data_name, is_header_skip=False):
+        self.dbname = dbname
+        self.target_table_name = target_table_name
+        self.import_data_name = import_data_name
+        self.is_header_skip = is_header_skip
+        _, raw_delimiter = os.path.splitext(import_data_name)
+        if raw_delimiter == '.csv':
+            self.delimiter = ','
+        else:
+            raise ValueError('Import file should be csv or tsv.')
+
+    def read_import_file(self):
+        with open(self.import_data_name, 'r') as f:
+            reader = csv.reader(f, delimiter=self.delimiter)
+            if self.is_header_skip:
+                header = next(reader)
+            return [i for i in reader]
+
+    def pick_column_num(self, import_data):
+        columns = []
+        for raw in import_data:
+            columns.append(len(raw))
+        if len(set(columns)) == 1:
+            return columns[0]
+        else:
+            raise ValueError('this import files has diffrenect column numbers.')
+
+    def insert_csv_file(self):
+        input_file = self.read_import_file()
+        column = self.pick_column_num(input_file)
+        val_questions = ['?' for i in range(column)]
+        cur.executemany("insert into {0} values ({1})".format(self.target_table_name, ','.join(val_questions)), input_file)
+
+if __name__ == '__main__':
+    sql = ImportSQLite(
+        dbname=dbname,
+        target_table_name=target_table_name,
+        import_data_name=import_table_name,
+        is_header_skip= is_header_skip,
+    )
+
+    conn = sqlite3.connect(sql.dbname)
+    conn.text_factory = str #use  UTF-8
+    cur = conn.cursor()
+
+    sql.insert_csv_file()
+
+    conn.commit()
+    conn.close()
